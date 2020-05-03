54c8538664327132964fc02be03d36f4a1324d9c
SadRavioli <56263170+SadRavioli@users.noreply.github.com>
2020-05-03 17:16:06 +0100

    Update binarytree.h

--- LabellingApplication/app/binarytree.h
+++ LabellingApplication/app/binarytree.h
@@ -10,7 +10,12 @@ class BinaryTree
 public:
 
     QStringList traversal;
-
+    
+    /**
+     * @brief Creates struct model for nodes
+     *
+     * @param parent
+     */
     struct TreeNode
     {
         QString data;
@@ -18,20 +23,79 @@ public:
         struct TreeNode *left;
         struct TreeNode *right;
     };
-
+    
     TreeNode *root;
-
+    
+    /**
+     * @brief Constructor for BinaryTree class
+     *
+     * @param parent
+     */
     BinaryTree();
-
+    
+    /**
+     * @brief Function to insert node into tree
+     * Each node stores filename and the time it was created
+     *
+     * @param parent
+     */
     void insertNode(QString, QString);
+    /**
+     * @brief Function to remove node from tree
+     * Takes filename and calls deleteNode function with root
+     *
+     * @param parent
+     */
     void remove(QString);
+    /**
+     * @brief Recursive function that goes through each node
+     * and then calls makeDeletion when the node is found
+     *
+     * @param parent
+     */
     void deleteNode(QString, TreeNode *&);
+    /**
+     * @brief Function to delete nodes
+     *
+     * @param parent
+     */
     void makeDeletion(TreeNode *&);
+    /**
+     * @brief Calls displayInOrder function
+     *
+     * @param parent
+     */
     void showNodeInOrder(void);
+    /**
+     * @brief Calls reverseInOrder function
+     *
+     * @param parent
+     */
     void showNodeInReverseOrder(void);
+    /**
+     * @brief Recursive function that goes left as far as it can, before adding
+     * node to traversal list, then right
+     *
+     * @param parent
+     */
     void displayInOrder(TreeNode *);
+    /**
+     * @brief Recursive function like InOrder but reverse. Right, add to list, left.
+     *
+     * @param parent
+     */
     void reverseInOrder(TreeNode *);
+    /**
+     * @brief Function that searches for node of particular value. Returns true or false
+     *
+     * @param parent
+     */
     bool searchNode(QString);
+    /**
+     * @brief Function to return traversal list.
+     *
+     * @param parent
+     */
     QStringList returnTraversal(void);
 
 };
